ee873c30f4fb908b6eef0a5bbe79ed8aaac4cba7
shubham88fru <shubham88fru@yahoo.com>
2026-02-20 21:25:28 -0600

    generic blocking queue

---
+++ multithreading/src/main/java/org/learning/jmthd/multithreading/blockingqueue/BlockingQueue.java
@@ -0,0 +1,36 @@
+package org.learning.jmthd.multithreading.blockingqueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BlockingQueue<T> {
+
+    private List<T> queue = new LinkedList<T>();
+
+    private int limit = 10;
+
+    public synchronized void put(T item) {
+        while (queue.size() == limit) {
+            try {
+                wait();
+            } catch (InterruptedException e) {}
+        }
+        if (queue.isEmpty()) {
+            notifyAll();
+        }
+        queue.add(item);
+    }
+
+    public synchronized T take() throws InterruptedException {
+        while (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {}
+        }
+        if (queue.size() == limit) {
+            notifyAll();
+        }
+        return queue.remove(0);
+    }
+
+}
